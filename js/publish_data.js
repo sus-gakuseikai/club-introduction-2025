f34754d53050365b5b6bb61210b8c06dc3531921
mi-1200 <kafupanpakazu2434@gmail.com>
2025-02-26 20:12:10 +0900

    Add files via upload

---
+++ js/publish_data.js
@@ -0,0 +1,36 @@
+const publish_info = [
+  {
+    category: '総務系',
+    clubs: [
+      { clubName: '学生会',
+        id: 'gakuseikai',//intro_pngの中の名前。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: 'X(旧Twitter)',
+              url: 'https://x.com/suwa_gakuseikai?s=20'
+            },
+        ]
+      },
+    ]
+  },
+  {
+    category: '運動系',
+    clubs: [
+      
+    ]
+  },
+  {
+    category: "文化系",
+    clubs: [
+    
+    ]
+  },
+  {
+    category: "外部団体",
+    clubs: [
+
+    ]
+  },
+]
