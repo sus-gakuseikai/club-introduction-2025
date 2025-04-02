6f9bc005877204cfc120c220fb570e08610d0e8b
mi-1200 <kafupanpakazu2434@gmail.com>
2025-04-02 20:49:08 +0900

    Update publish_data.js

--- js/publish_data.js
+++ js/publish_data.js
@@ -64,6 +64,88 @@ const publish_info = [
             },
         ]
       },
+      { clubName: 'クライミング部',
+        id: 'softball_baseball',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: 'クライミング部X（旧Twitter）',
+              url: 'https://x.com/SuwarikaClimb'
+            },
+        ]
+      },
+      { clubName: 'クライミング部',
+        id: 'climbing',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: 'クライミング部X（旧Twitter）',
+              url: 'https://x.com/SuwarikaClimb'
+            },
+        ]
+      },
+       { clubName: '弓道部',
+        id: 'kyudo',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+            },
+        ]
+      },
+      { clubName: 'サバイバルゲーム部',
+        id: 'survival',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: 'サバイバルゲーム部Twitter',
+              url: 'https://x.com/sus_sarvival?s=21&t=1DRjJxI9fQyVarRTvQ_K6A'
+            },
+        ]
+      },
+      { clubName: '剣道部',
+        id: 'kendo',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: '剣道部連絡先',
+              url: 'kendo.suwarika@gmail.com'
+            },
+        ]
+      },
+       { clubName: 'ゴルフ部',
+        id: 'golf',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: 'ゴルフ部 Instagram',
+              url: 'https://www.instagram.com/sus_golfclub/'
+            },
+        ]
+      },
+       { clubName: 'サッカー部',
+        id: 'soccer',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+            },
+        ]
+      },
+       { clubName: 'テニス部',
+        id: 'tennis',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+            },
+        ]
+      },
     ]
   },
   {
@@ -100,6 +182,63 @@ const publish_info = [
         links: [
             {
             },
+        ]
+      },
+    　{ clubName: 'ボードゲーム部',
+        id: 'board_game',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+            },
+        ]
+      },
+       { clubName: 'ロボット研究部',
+        id: 'robot',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+            },
+        ]
+      },
+       { clubName: 'e-Sporrts部',
+        id: 'e-Sporrts',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: true,
+        movieUrl: '',
+        links: [
+            {
+            },
+        ]
+      },
+       { clubName: '写真部',
+        id: 'photograph',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: '写真部X',
+              url: 'https://x.com/suwarika_photo'
+            },
+        ]
+      },
+       { clubName: '軽音学部',
+        id: 'K-ON',//intro_pngの中のファイル名。拡張端子はpngのみ
+        isMovie: false,
+        movieUrl: '',
+        links: [
+            {
+              label: '軽音部 X',
+              url: 'https://x.com/sus_schoolband'
+            },
+            {
+              label: '軽音部 YouTube',
+              url: 'https://www.youtube.com/channel/UCpUM8-AZ0zxWZbyvgnzLMdg',
+            }
+            {
+              label: '軽音部 Instagram',
+              url: 'https://www.instagram.com/sus_schoolband/'
+            }
         ]
       },
     ]
